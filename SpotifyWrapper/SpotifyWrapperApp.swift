f556675e318f2d01a6cd1ee54ed6fdd697a9c363
lburgess-31 <minvest.lburgess@outlook.com>
2024-01-28 20:58:28 -0500

    Initial Commit

---
+++ SpotifyWrapper/SpotifyWrapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyWrapperApp.swift
+//  SpotifyWrapper
+//
+//  Created by Leah on 2024-01-28.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyWrapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
